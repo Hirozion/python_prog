73b199afbd5a44edf91a5fc2ff588d3ced92fdf5
Deprez Martin <Deprez.Martin@gmx.com>
2022-01-11 19:36:01 +0100

    adding game test for fun

--- test1/test.py
+++ test1/test.py
@@ -1,1 +1,20 @@
-print("hello woorld")
+import random
+
+secret = random.randint(0,10)
+life = 10
+
+print(secret)
+nb = int(input("Rentrez un nombre svp : "))
+
+while nb != secret:
+    if nb == secret:
+        print("GG !")
+        exit(1)
+    elif life == 0:
+        print("Game Over L")
+        exit(1)
+    else:
+        nb = int(input("Rentrez un nombre svp : "))
+        life -= 1
+
+#make a class with this add difficulty lvl 1 - 2 -3 
